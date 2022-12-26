6953b03ead1df44fe53d26a066807a3805fe1516
rqphy <raphael.ferreira@hetic.net>
2022-12-26 19:51:26 +0100

    :construction: add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.scss'
 import Text from './Components/Text/Text'
+import Header from './Components/Header/Header'
 
 function App() {
 
   return (
     <div className="App">
+      <Header />
       <Text tag="h2" size="xl" color='main' >Hello World 2</Text>
       <Text tag="p" size="l" typo='secondary' >Hello World 2</Text>
       <Text tag="h5" size="m" typo='secondary' >Hello World 2</Text>
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import Text from '../Text/Text'
+import './style.scss'
+
+export default function Header()
+{
+    return <>
+        <header className='header'>
+            <Text tag='p'>Ferreira Raphaël</Text>
+        </header>
+    </>
+}
