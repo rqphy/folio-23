3d6237fbd1302f53ed0daa4d92f2f91719e4229b
rqphy <raphael.ferreira@hetic.net>
2022-12-26 20:21:33 +0100

    :lipstick: header > add default style

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,6 +6,12 @@ export default function Header()
     return <>
         <header className='header'>
             <Text tag='p'>Ferreira Raphaël</Text>
+            <Text tag='p'>Front-end / Creative developper</Text>
+            <nav className='header__nav'>
+                <Text tag='p' size='s'>Projets</Text>
+                <Text tag='p' size='s'>À Propos</Text>
+                <Text tag='p' size='s'>Contact</Text>
+            </nav>
         </header>
     </>
 }
