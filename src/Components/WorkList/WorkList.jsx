d8dda0bf31435019c28d3cae1e2272e697817800
Raphael <raphael.ferreira@hetic.net>
2022-12-31 21:41:08 +0100

    :sparkles: worklist > find a way to make each project element look at the center of the circle

--- src/Components/WorkList/WorkList.jsx
+++ src/Components/WorkList/WorkList.jsx
@@ -2,30 +2,42 @@ import './WorkList.scss'
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { useThree, extend } from '@react-three/fiber'
-import { useEffect, useState } from 'react'
+import { useRef, useEffect } from 'react'
 
 extend({ OrbitControls })
 
 // Math.PI * 2 / itemCount
 
+const numberOfProjects = 8
+const circleRadius = 6
+
 export default function WorkList()
 {
     const { camera, gl } = useThree()
+    const projects = useRef([])
+
+    useEffect(() =>
+    {
+        for(const project of projects.current)
+        {
+            project.lookAt(0, 0, 0)
+        } 
+    }, [])
 
     return <>
         <orbitControls args={ [ camera, gl.domElement ] } />
 
 
         {
-            [...Array(20)].map((item, index) => (
-                <mesh 
+            [...Array(numberOfProjects)].map((item, index) => (
+                <mesh
+                    ref={ (el) => projects.current[index] = el }
                     position={[
-                        Math.cos((Math.PI * 2 / 20) * index) * 5,
+                        Math.cos((Math.PI * 2 / numberOfProjects) * index) * circleRadius,
                         0,
-                        Math.sin((Math.PI * 2 / 20) * index) * 5,
+                        Math.sin((Math.PI * 2 / numberOfProjects) * index) * circleRadius,
                     ]}
-                    // position={[ 5, 0, 4 ]}
-                    // rotation-y={ Math.PI * 0.2 }
+                    key={ index }
                 >
                     <boxGeometry args={[ 4, 5, 1 ]} />
                     <meshBasicMaterial color="blue" />
