3437174c55b559b9af8b764d220f967be1b49ee9
Raphael <raphael.ferreira@hetic.net>
2022-12-31 21:14:56 +0100

    :sparkles: worklist > find formula to place elements in a circle

--- src/Components/WorkList/WorkList.jsx
+++ src/Components/WorkList/WorkList.jsx
@@ -2,39 +2,36 @@ import './WorkList.scss'
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { useThree, extend } from '@react-three/fiber'
+import { useEffect, useState } from 'react'
 
 extend({ OrbitControls })
+
+// Math.PI * 2 / itemCount
 
 export default function WorkList()
 {
     const { camera, gl } = useThree()
 
-
     return <>
         <orbitControls args={ [ camera, gl.domElement ] } />
 
-        <mesh 
-            position={[
-                Math.cos(Math.PI * 0.5) * 5,
-                5,
-                Math.sin(Math.PI * 0.5) * 5,
-            ]}
-            rotation-y={ Math.PI * 0.5 }
-        >
-            <boxGeometry args={[ 4, 5, 1 ]} />
-            <meshBasicMaterial color="blue" />
-        </mesh>
-        <mesh position={[ 0, 0, 5 ]}>
-            <boxGeometry args={[ 4, 5, 1 ]} />
-            <meshBasicMaterial color="red" />
-        </mesh>
-        <mesh position={[ 0, 0, 5 ]}>
-            <boxGeometry args={[ 4, 5, 1 ]} />
-            <meshBasicMaterial color="green" />
-        </mesh>
-        <mesh position={[ 0, 0, 5 ]}>
-            <boxGeometry args={[ 4, 5, 1 ]} />
-            <meshBasicMaterial color="pruple" />
-        </mesh>
+
+        {
+            [...Array(20)].map((item, index) => (
+                <mesh 
+                    position={[
+                        Math.cos((Math.PI * 2 / 20) * index) * 5,
+                        0,
+                        Math.sin((Math.PI * 2 / 20) * index) * 5,
+                    ]}
+                    // position={[ 5, 0, 4 ]}
+                    // rotation-y={ Math.PI * 0.2 }
+                >
+                    <boxGeometry args={[ 4, 5, 1 ]} />
+                    <meshBasicMaterial color="blue" />
+                </mesh>
+            ))
+        }
+
     </>
 }
