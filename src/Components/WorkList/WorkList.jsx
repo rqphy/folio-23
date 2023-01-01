bb96ba8e0ba80c49b62b585de0b2caaccd10d3ce
Raphael <raphael.ferreira@hetic.net>
2023-01-01 16:24:57 +0100

    :tada: worklist > init controls with presentationControls from drei and limit the rotation

--- src/Components/WorkList/WorkList.jsx
+++ src/Components/WorkList/WorkList.jsx
@@ -3,6 +3,7 @@ import './WorkList.scss'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { useThree, extend } from '@react-three/fiber'
 import { useRef, useEffect } from 'react'
+import { PresentationControls } from '@react-three/drei'
 
 extend({ OrbitControls })
 
@@ -25,25 +26,33 @@ export default function WorkList()
     }, [])
 
     return <>
-        <orbitControls args={ [ camera, gl.domElement ] } />
+        {/* <orbitControls args={ [ camera, gl.domElement ] } /> */}
+        <PresentationControls
+            global
+            // rotation={[ 0.13, 0.1, 0 ]}
+            polar={[ 0, 0 ]}
+            // azimuth={[ -1, 0.75 ]}
+            // config={{ mass: 2, tension: 400 }}
+            // snap={{ mass: 4, tension: 400 }}
+        >
 
-
-        {
-            [...Array(numberOfProjects)].map((item, index) => (
-                <mesh
-                    ref={ (el) => projects.current[index] = el }
-                    position={[
-                        Math.cos((Math.PI * 2 / numberOfProjects) * index) * circleRadius,
-                        0,
-                        Math.sin((Math.PI * 2 / numberOfProjects) * index) * circleRadius,
-                    ]}
-                    key={ index }
-                >
-                    <boxGeometry args={[ 4, 5, 1 ]} />
-                    <meshBasicMaterial color={`hsl(${Math.random() * 360}, 100%, 75%)`} />
-                </mesh>
-            ))
-        }
+            {
+                [...Array(numberOfProjects)].map((item, index) => (
+                    <mesh
+                        ref={ (el) => projects.current[index] = el }
+                        position={[
+                            Math.cos((Math.PI * 2 / numberOfProjects) * index) * circleRadius,
+                            0,
+                            Math.sin((Math.PI * 2 / numberOfProjects) * index) * circleRadius,
+                        ]}
+                        key={ index }
+                    >
+                        <boxGeometry args={[ 4, 5, 1 ]} />
+                        <meshBasicMaterial color={`hsl(${Math.random() * 360}, 100%, 75%)`} />
+                    </mesh>
+                ))
+            }
+        </PresentationControls>
 
     </>
 }
