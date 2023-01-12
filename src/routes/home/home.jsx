81ca48148a453978461b7944843f1205a95133fc
rqphy <raphael.ferreira@hetic.net>
2023-01-12 15:40:01 +0100

    :sparkles: home > add experiences and certificates

--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -25,6 +25,41 @@ export default function Home()
                     <p className='about__pitch'>Je suis Raphaël Ferreira, un développeur front de 21 ans diplômé d'un Bachelor web à Hetic. Je réalise une multitude de projets que je mets en ligne sur github.</p>
                 </div>
             </section>
+            <section className='experiences'>
+                <Text>Experiences</Text>
+                <ul className="experiences__content">
+                    <li className='experiences__item'>
+                        <Text>Lactel</Text>
+                        <Text>Wonderstudios</Text>
+                        <Text>2021</Text>
+                    </li>
+                    <li className='experiences__item'>
+                        <Text>Harmonie Mutuelle</Text>
+                        <Text>Wonderstudios</Text>
+                        <Text>2021</Text>
+                    </li>
+                    <li className='experiences__item'>
+                        <Text>Addiction Agency</Text>
+                        <Text>Wonderstudios</Text>
+                        <Text>2021</Text>
+                    </li>
+                </ul>
+            </section>
+            <section className="certificates">
+                <Text>Diplômes</Text>
+                <ul className='certificates__content'>
+                    <li className="certificates__item">
+                        <Text>Three.js Journey</Text>
+                        <Text>Bruno Simon</Text>
+                        <Text>2023</Text>
+                    </li>
+                    <li className="certificates__item">
+                        <Text>Web developper</Text>
+                        <Text>Hetic</Text>
+                        <Text>2019-2022</Text>
+                    </li>
+                </ul>
+            </section>
 
         </main>
     </>
