9c461fcd3c0b31b5f22e150da176778d80b7e2b7
rqphy <raphael.ferreira@hetic.net>
2023-01-28 11:58:39 +0100

    :sparkles: component > add link component to other pages

---
+++ src/components/link/link.jsx
@@ -0,0 +1,12 @@
+import './link.scss'
+import Text from '../text/text'
+import { Link as DOMLink } from 'react-router-dom'
+
+export default function Link({ to, children })
+{
+    return <div className='link'>
+        <DOMLink to={ to } >
+            { children }
+        </DOMLink>
+    </div>
+}
