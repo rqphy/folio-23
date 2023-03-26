65637f3218c1f39a6f56abfc5adea1e33720b497
rqphy <raphael.ferreira@hetic.net>
2023-03-26 15:19:43 +0200

    :sparkles: work > fetch readme

--- src/routes/work/work.jsx
+++ src/routes/work/work.jsx
@@ -1,10 +1,25 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import data from "./../../data/works.json"
 
 export default function Work() {
 	const { slug } = useParams()
 
+	const [readme, setReadme] = useState("")
+
 	useEffect(() => {
+		async function fetchReadme() {
+			const response = await fetch(
+				"https://api.github.com/repos/rqphy/TinyHeaven/readme"
+			)
+			const data = await response.json()
+			const readmeResponse = await fetch(data.download_url)
+			const readmeText = await readmeResponse.text()
+			console.log(readmeText)
+			setReadme(readmeText)
+		}
+
+		fetchReadme()
 		document.title = `Project ${slug}`
 	}, [])
 
